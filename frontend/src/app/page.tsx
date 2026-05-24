9ffa4ad27db3658fe32e67cb708930f3d0a6e0be
MasonD-007 <139420415+MasonD-007@users.noreply.github.com>
2026-05-23 23:05:51 -0700

    fixed dynamic secret loading

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+export const dynamic = 'force-dynamic';
+
 export default function HomePage() {
   const demoSecretMessage = process.env.DEMO_SECRET_MESSAGE || 'not configured';
 
